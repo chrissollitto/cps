3233069f45a3ea90f7a6c0f9aeaa732e281126d9
Chris Sollitto <chris.sollitto@stackadapt.com>
2025-03-19 09:41:45 -0400

    parameterize TreeNode

--- example.go
+++ example.go
@@ -6,18 +6,20 @@ import (
 	"github.com/chrissollitto/cps/iterator"
 )
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
+type TreeNode[T any] struct {
+	Val   T
+	Left  *TreeNode[T]
+	Right *TreeNode[T]
 }
 
-func main() {
-	exampleTree := &TreeNode{Val: 7, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 15, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20}}}
+type TreeNodeInt = TreeNode[int]
 
-	dfsIterator := &iterator.CPSIterator[TreeNode, int]{}
+func main() {
+	exampleTree := &TreeNodeInt{Val: 7, Left: &TreeNodeInt{Val: 3}, Right: &TreeNodeInt{Val: 15, Left: &TreeNodeInt{Val: 9}, Right: &TreeNodeInt{Val: 20}}}
+
+	dfsIterator := &iterator.CPSIterator[TreeNodeInt, int]{}
 	dfsIterator.Init(exampleTree,
-		func(node *TreeNode, cont func() int) int {
+		func(node *TreeNodeInt, cont func() int) int {
 			return dfsIterator.Run(node.Left, func() int {
 				return dfsIterator.Suspend(node.Val, func() int {
 					return dfsIterator.Run(node.Right, cont)
