b14781fba6fda17c98f46109f371ada87cccec35
Chris Sollitto <chris.sollitto@stackadapt.com>
2025-03-21 20:15:08 -0400

    Add fib and trib

--- example.go
+++ example.go
@@ -14,6 +14,14 @@ type TreeNode[T any] struct {
 
 type TreeNodeInt = TreeNode[int]
 
+type Fib struct {
+	n1, n2 int
+}
+
+type Nbonacci struct {
+	n []int
+}
+
 func main() {
 	exampleTree := &TreeNodeInt{Val: 7, Left: &TreeNodeInt{Val: 3}, Right: &TreeNodeInt{Val: 15, Left: &TreeNodeInt{Val: 9}, Right: &TreeNodeInt{Val: 20}}}
 
@@ -61,4 +69,39 @@ func main() {
 	for dfsPostorderIter.HasNext() {
 		fmt.Println(dfsPostorderIter.Next())
 	}
+
+	fmt.Println("----- Fibonacci Sequence ------")
+	fibIter := &iterator.CPSIterator[Fib, int]{}
+	fibIter.Init(&Fib{0, 1},
+		func(f *Fib, cont func() int) int {
+			return fibIter.Suspend(f.n1, func() int {
+				return fibIter.Run(&Fib{f.n2, f.n1 + f.n2}, cont)
+			})
+		})
+
+	for i := 0; i < 20; i++ {
+		fmt.Println(fibIter.Next())
+	}
+
+	fmt.Println("----- Nbonacci Sequence ------")
+	nbonacciIter := &iterator.CPSIterator[Nbonacci, int]{}
+	nbonacciIter.Init(&Nbonacci{[]int{0, 0, 1}},
+		func(f *Nbonacci, cont func() int) int {
+			return nbonacciIter.Suspend(f.n[0], func() int {
+				sum := 0
+				for i, x := range f.n {
+					sum += x
+					if i > 0 {
+						f.n[i-1] = x
+					}
+				}
+				f.n[len(f.n)-1] = sum
+				return nbonacciIter.Run(f, cont)
+			})
+		})
+
+	for i := 0; i < 20; i++ {
+		fmt.Println(nbonacciIter.Next())
+	}
+
 }
